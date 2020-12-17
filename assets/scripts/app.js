1ed479ff8295d0542634508220272aead5b210ea
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-17 13:14:46 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,36 @@ const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
+// const userInput = addMovieModal.getElementsByTagName('input');
+const entryTextSection = document.getElementById('entry-text');
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className ='movie-element';
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -16,8 +46,45 @@ const toggleMovieModal = () => { //function () {}
   toggleBackdrop();
 }; 
 
-const cancelAddMovie = () => {
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = '';
+  }
+};
+
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue >5
+    ) {
+      alert('Please enter valid values (rating between 1 and 5).');
+      return;
+    }
+
+    const newMovies = {
+      title: titleValue,
+      image: imageUrlValue,
+      rating: ratingValue
+    };
+
+    movies.push(newMovies);
+    console.log(movies);
+    toggleMovieModal();
+    clearMovieInput();
+    renderNewMovieElement(newMovies.title, newMovies.image, newMovies.rating);
+    updateUI();
 };
 
 const backdropClickHandler = () => {
@@ -26,5 +93,6 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
 
